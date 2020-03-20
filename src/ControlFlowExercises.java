96c9861706351e0140c03b7d9856f594198efbba
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-03-20 15:15:56 -0500

    completed next exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,14 +15,25 @@ public class ControlFlowExercises {
 
 //        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 //        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-        long i = 0;
-        do {
-            System.out.printf("\t%d\n", i);
-            i += 2;
-        } while (i < 101);
-        System.out.print("\n");
+//        long i = 0;
+//        do {
+//            System.out.printf("\t%d\n", i);
+//            i += 2;
+//        } while (i < 101);
+//        System.out.print("\n");
 
 //        Alter your loop to count backwards by 5's from 100 to -10.
+
+        long i = 100;
+        do {
+            System.out.printf("\t%d\n", i);
+            i -= 5;
+        } while (i > -11);
+        System.out.print("\n");
+
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+
+
 
 
 
